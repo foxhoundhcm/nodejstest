dfbd25872919e44d7be46f9fad93f04bfe266f24
unknown <foxhoundhcm@gmail.com>
2017-06-02 04:38:52 +0800

    add graphql simple

---
+++ db.js
@@ -0,0 +1,77 @@
+import Sequelize from 'sequelize';
+import _ from 'lodash';
+import Faker from 'faker';
+
+// const Conn = new Sequelize(
+//   'relay',
+//   'postgres',
+//   'postgres',
+//   {
+//     dialect:'postgres',
+//     host:'localhost'
+//   }
+// );
+
+const Conn = new Sequelize('test', 'root', '159951', {
+    host: '192.168.1.200',
+    port: '3036',
+    dialect: 'mariadb',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+});
+
+const Person = Conn.define('person',{
+  firstName:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  email:{
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate:{
+      isEmail: true
+    }
+  }
+
+});
+
+const Post = Conn.define('post',{
+  title:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content:{
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+
+});
+
+Person.hasMany(Post);
+Post.belongsTo(Person);
+
+Conn.sync({force:true}).then(()=>{
+  _.times(10,()=>{
+    return Person.create({
+      firstName: Faker.name.firstName(),
+      lastName: Faker.name.lastName(),
+      email: Faker.internet.email(),
+    }).then(person =>{
+      return person.createPost({
+        title: 'Sample title by ${person.firstName}',
+        content: 'This is a sample article'
+      });
+    });
+  });
+  console.log('done!');
+});
+
+export default Conn;
---
+++ graphqlserver.js
@@ -0,0 +1,33 @@
+import koa from 'koa';
+import koaRouter from 'koa-router';
+import koaBody from 'koa-bodyparser';
+import { graphqlKoa } from 'graphql-server-koa';
+import graphqlHTTP from 'koa-graphql';
+
+import Schema from './schema';
+
+// var koa = require('koa');
+// var koaRouter = require('koa-router');
+// var koaBody = require('koa-bodyparser');
+// var { graphqlKoa } = require('graphql-server-koa');
+
+// var Schema = require('./schema');
+
+const app = new koa();
+const router = new koaRouter();
+const PORT = 3000;
+app.use(koaBody());
+// router.post('/graphql', graphqlKoa({ schema: Schema }));
+// router.get('/graphql', graphqlKoa({ schema: Schema }));
+
+router.all('/graphql', graphqlHTTP({
+  schema: Schema,
+  pretty: true,
+  graphiql: true
+}));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(PORT,()=>{
+  console.log('App listening on port ${PORT}');
+});
